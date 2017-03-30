d896f039ab3568cfe5decc32809a2098ff7715da
GDmail <GDmail91@gmail.com>
2017-03-30 18:32:45 +0900

    *fix plugin build task timing & add lantern sdk dependency in plugin

--- src/main/java/hello/thinkcode/DemoPlugin.java
+++ src/main/java/hello/thinkcode/DemoPlugin.java
@@ -5,6 +5,13 @@ import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.Task;
 import org.gradle.api.tasks.TaskContainer;
+import org.gradle.api.artifacts.dsl.DependencyHandler; 
+import org.gradle.api.artifacts.Dependency;
+import org.gradle.api.artifacts.DependencySet;
+import org.gradle.api.artifacts.Configuration;
+import org.gradle.api.artifacts.ConfigurationContainer;
+import org.gradle.api.artifacts.DependencyResolutionListener;
+import org.gradle.api.artifacts.ResolvableDependencies;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -22,14 +29,23 @@ import java.util.Map;
 import java.util.Set;
 import java.util.List;
 import java.io.FileInputStream;
+import java.lang.NullPointerException;
 
 public class DemoPlugin implements Plugin<Project> {
+	public static final String CONF_LANTERN_SDK = "lantern";
+	DependencySet compileDeps;
+
     @Override
     public void apply(Project project) {
 
         Set<Project> allProject = project.getAllprojects();
         System.out.println("All Project: "+allProject.toString());
+        System.out.println("This Project: "+project.toString());
+        System.out.println(project.getDependencies());
 
+        compileDeps = project.getConfigurations().getByName("compile").getDependencies();
+        compileDeps.add(project.getDependencies().create("com.lantern.lantern:app-debug:0.1.13@aar"));
+		
         project.afterEvaluate(new Action<Project>() {
             @Override
             public void execute(Project project) {
@@ -38,9 +54,9 @@ public class DemoPlugin implements Plugin<Project> {
         		System.out.println("Task Container: "+tc.toString());
                 
 		        //Task complieTask = tc.getByPath(":app:compileDebugSources");
-		        Task complieTask = tc.getByPath(":app:compileDebugJavaWithJavac");
+		        Task complieTask = tc.getByPath(":app:mockableAndroidJar");
 		        System.out.println("Task Name: "+complieTask.getName() + ", Task Desc: "+complieTask.getDescription());
-		        complieTask.doLast(new Action<Task>() {
+		        complieTask.doFirst(new Action<Task>() {
 		        	@Override
 		        	public void execute(Task task) {
 		        		System.out.println("Task Name: "+task.getName() + ", Task Desc: "+task.getDescription());
@@ -68,6 +84,7 @@ public class DemoPlugin implements Plugin<Project> {
 
 				                	// If found activity node do change .class file
 				                    Element eElement = (Element) nNode;
+				                	// TODO 앱의 액티비티인지 패키지명으로 검사할 것 
 				                    String activityName = getActivityName(eElement);
 				                    if (activityName != null) {
 				                    	activityName = activityName.replace(".", "/");
@@ -76,6 +93,7 @@ public class DemoPlugin implements Plugin<Project> {
 
 					                    // File location it
 					                    String buildLocation = "app/build/intermediates/classes/debug/";
+					                    // TODO 해당 앱의 패키지로 변경할것
 					                    File location = new File(buildLocation + "com/example/bigasslayout/bigasslayout");
 					                    if (location.isDirectory()) {
 					                    	for (String tmp : location.list()) {
