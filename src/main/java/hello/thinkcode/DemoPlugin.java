e1aa32a684327cb76517ac382cb14553110d0f27
yky <jratul@nate.com>
2017-04-20 23:14:35 +0900

    *chg version 0.1.7 sdk url to rabbitmq

--- src/main/java/hello/thinkcode/DemoPlugin.java
+++ src/main/java/hello/thinkcode/DemoPlugin.java
@@ -42,6 +42,7 @@ public class DemoPlugin implements Plugin<Project> {
         System.out.println("All Project: "+allProject.toString());
         // 실시간 의존성 추가
         compileDeps = project.getConfigurations().getByName("compile").getDependencies();
+        compileDeps.add(project.getDependencies().create("com.lantern:lantern-sdk:0.1.7"));
         //compileDeps.add(project.getDependencies().create("com.lantern.lantern:app-debug:0.1.13@aar"));
         project.afterEvaluate(new Action<Project>() {
             @Override
