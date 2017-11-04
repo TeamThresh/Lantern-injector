f60bfdf7ed2332389125973402d5d791cdd19cfe
Youngsoo Ryu <gdmail91@gmail.com>
2017-11-05 00:24:50 +0900

    *update 0.1.67

--- src/main/java/com/lantern/lantern/LanternPlugin.java
+++ src/main/java/com/lantern/lantern/LanternPlugin.java
@@ -37,7 +37,7 @@ import java.util.regex.Pattern;
 public class LanternPlugin implements Plugin<Project> {
 	public static final String CONF_LANTERN_SDK = "lantern";
 	DependencySet compileDeps;
-	private final String SDK_INFO_FOR_DEPENDENCY = "com.lantern:lantern-sdk:0.1.66";
+	private final String SDK_INFO_FOR_DEPENDENCY = "com.lantern:lantern-sdk:0.1.67";
 
     @Override
     public void apply(Project project) {
