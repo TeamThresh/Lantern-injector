ae7aa64a75d343caaa399253224e2fccf9c5f090
유영수 <gdmail91@gmail.com>
2017-11-05 22:05:47 +0900

    *update refactoring, buildType changed

--- src/main/java/com/lantern/lantern/LanternPlugin.java
+++ src/main/java/com/lantern/lantern/LanternPlugin.java
@@ -42,11 +42,44 @@ public class LanternPlugin implements Plugin<Project> {
     @Override
     public void apply(Project project) {
 
+		System.out.println("Test Version");
         Set<Project> allProject = project.getAllprojects();
         System.out.println("All Project: "+allProject.toString());
-        // 실시간 의존성 추가
+
         compileDeps = project.getConfigurations().getByName("compile").getDependencies();
-        compileDeps.add(project.getDependencies().create(SDK_INFO_FOR_DEPENDENCY));
+        if (!hasDependency(compileDeps)) {
+			dynamicDependencyInject(compileDeps, project, SDK_INFO_FOR_DEPENDENCY);
+		}
+
+		project.afterEvaluate(new Action<Project>() {
+			@Override
+			public void execute(Project project) {
+				System.out.println("After Evaluate!!!");
+				TaskContainer tc = project.getTasks();
+				System.out.println("Task Container: "+tc.toString());
+
+				Iterator<Task> it = tc.iterator();
+				while(it.hasNext()) {
+					// Processing when Release Task existing in compile tasks
+					Task compileTask = it.next();
+
+					// Find Javac Task
+					Pattern javacPattern = Pattern.compile(":app:compile(\\w*)(Release|Debug)JavaWithJavac");
+					Matcher javacMatcher = javacPattern.matcher( compileTask.toString() );
+					if( javacMatcher.find() ) {
+						//if (compileTask.getPath().equals(":app:compileDebugJavaWithJavac")) {
+						//Task complieTask = tc.getByPath(":app:lint");
+						findBuildType(compileTask);
+					}
+				}
+			}
+    	});
+        System.out.println("LanternPlugin Applied");
+
+    }
+
+    private static boolean hasDependency(DependencySet compileDeps) {
+    	// 의존성 확인
         //compileDeps.add(project.getDependencies().create("com.lantern.lantern:app-debug:0.1.13@aar"));
         boolean hasDependency = false;
         Iterator<Dependency> it = compileDeps.iterator();
@@ -59,120 +92,125 @@ public class LanternPlugin implements Plugin<Project> {
             }
         }
 
-        if (!hasDependency)
-        	compileDeps.add(project.getDependencies().create(SDK_INFO_FOR_DEPENDENCY));
-        project.afterEvaluate(new Action<Project>() {
-            @Override
-            public void execute(Project project) {
-                System.out.println("After Evaluate!!!");
-                TaskContainer tc = project.getTasks();
-        		//System.out.println("Task Container: "+tc.toString());
-
-			    Iterator<Task> it = tc.iterator();
-		        while(it.hasNext()) {
-		        	// Processing when Release Task existing in compile tasks
-		        	Task compileTask = it.next();
-
-	            	Pattern flavorPattern = Pattern.compile(":app:compile(\\w+)(Release|Debug)JavaWithJavac");
-	        		Matcher flavorMatcher = flavorPattern.matcher( compileTask.toString() );
-	        		if( flavorMatcher.find() ) {
-		        		//if (compileTask.getPath().equals(":app:compileDebugJavaWithJavac")) {
-		            	//Task complieTask = tc.getByPath(":app:lint");
-				        System.out.println("Task Name: "+compileTask.getName() + ", Task Desc: "+compileTask.getDescription());
-				        compileTask.doLast(new Action<Task>() {
-				        	@Override
-				        	public void execute(Task task) {
-				        		System.out.println("Task Name: "+task.getName() + ", Task Desc: "+task.getDescription());
-
-				        		String flavor = "";
-				        		String buildType = "";
-								Pattern flavorPattern = Pattern.compile(":app:compile(\\w+)(Release|Debug)JavaWithJavac");
-				        		Matcher flavorMatcher = flavorPattern.matcher( task.toString() );
-				        		if( flavorMatcher.find() ) {
-				        			flavor = flavorMatcher.group(1).toLowerCase();
-				        			Pattern bulidTypePattern = Pattern.compile(":app:compile"+flavorMatcher.group(1)+"(\\w+)JavaWithJavac");
-				        			Matcher buildTypeMatcher = bulidTypePattern.matcher(task.toString());
-				        			if (buildTypeMatcher.find())
-				        				buildType = buildTypeMatcher.group(1).toLowerCase();
-				        		}
-				        		// Get Activity List on Manifest.xml
-				                //String fileName = "app/build/intermediates/manifests/full/debug/AndroidManifest.xml";
-				                String fileName = "app/build/intermediates/manifests/full/";
-				                if (!flavor.equals("")) {
-				                	fileName += flavor+"/";
-				                }
-				                fileName += buildType+"/AndroidManifest.xml";
-
-						        try {
-						            File fXmlFile = new File(fileName);
-						            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-						            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-						            Document doc = dBuilder.parse(fXmlFile);
-						            doc.getDocumentElement().normalize();
-						            String packageName = doc.getDocumentElement().getAttribute("package");
-						            packageName = packageName.replace(".", "/");
-
-						            // Search Element name that have 'activity' in 'application' 
-						            System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
-						            NodeList applicationList = doc.getElementsByTagName("application");
-						            NodeList activityList = applicationList.item(0).getChildNodes();
-						            System.out.println("-----------------------");
-
-						            // This find 'activity'
-						            for (int index = 0; index < activityList.getLength(); index++) {
-						                Node nNode = activityList.item(index);
-						                if (nNode.getNodeType() == Node.ELEMENT_NODE
-						                    && nNode.getNodeName().equals("activity")) {
-
-						                	// If found activity node do change .class file
-						                    Element eElement = (Element) nNode;
-						                	// TODO 앱의 액티비티인지 패키지명으로 검사할 것 
-						                    String activityName = getActivityName(eElement);
-						                    if (activityName != null) {
-						                    	activityName = activityName.replace(".", "/");
-							                    System.out.println("Activity name : " + activityName);
-							                    System.out.println("--- do transform ---");
-
-							                    // File location it
-							                    String buildLocation = "app/build/intermediates/classes/";
-							                    if (!flavor.equals("")) {
-								                	buildLocation += flavor+"/";
-								                }
-								                buildLocation += buildType+"/";
-							                    //String buildLocation = "app/build/intermediates/classes/debug/";
-							                    /*
-							                    // TODO 해당 앱의 패키지로 변경할것
-							                    File location = new File(buildLocation + "com/example/bigasslayout/bigasslayout");
-							                    if (location.isDirectory()) {
-							                    	for (String tmp : location.list()) {
-														System.out.println(tmp);
-													}
-												}*/
-
-							                    System.out.println("Bulid Location : " + buildLocation + activityName);
-							                    new Transformer().doTransform(
-							                    	buildLocation + activityName+".class", 
-							                    	new FileInputStream(buildLocation + activityName + ".class")
-							                    	);
-						                    }
-						                }
-						            }
-						        } catch (Exception e) {
-						            e.printStackTrace();
-						        }
-				        	}
-				        });
-		            }
-		        }
-            }
-        });
-
-        //*/
-
-        System.out.println("LanternPlugin Applied");
-
+        return hasDependency;
     }
 
+    private static void dynamicDependencyInject(DependencySet compileDeps, Project project, String name) {
+        // 실시간 의존성 추가
+		compileDeps.add(project.getDependencies().create(name));
+    }
+
+    private static void findBuildType(Task compileTask) {
+    	System.out.println("Task Name: "+compileTask.getName() + ", Task Desc: "+compileTask.getDescription());
+        compileTask.doLast(new Action<Task>() {
+        	@Override
+        	public void execute(Task task) {
+        		System.out.println("Task Name: "+task.getName() + ", Task Desc: "+task.getDescription());
+
+        		// Find Flavor and Build type
+        		String flavor = "";
+        		String buildType = "";
+				Pattern flavorPattern = Pattern.compile(":app:compile(\\w+)(Release|Debug)JavaWithJavac");
+        		Matcher flavorMatcher = flavorPattern.matcher( task.toString() );
+        		if( flavorMatcher.find() ) {
+        			// If exist flavor 
+        			flavor = flavorMatcher.group(1).toLowerCase();
+        			Pattern bulidTypePattern = Pattern.compile(":app:compile"+flavorMatcher.group(1)+"(\\w+)JavaWithJavac");
+        			Matcher buildTypeMatcher = bulidTypePattern.matcher(task.toString());
+        			if (buildTypeMatcher.find())
+        				buildType = buildTypeMatcher.group(1).toLowerCase();
+        		} else {
+        			// no flavor
+        			Pattern bulidTypePattern = Pattern.compile(":app:compile(\\w+)JavaWithJavac");
+        			Matcher buildTypeMatcher = bulidTypePattern.matcher(task.toString());
+        			if (buildTypeMatcher.find())
+        				buildType = buildTypeMatcher.group(1).toLowerCase();
+        		}
+
+        		// Get Activity List on Manifest.xml
+        		NodeList activityList = getActivityList(getManifestLocation(flavor, buildType));
+
+                // File location it
+                String buildLocation = "app/build/intermediates/classes/";
+                if (!flavor.equals("")) {
+                	buildLocation += flavor+"/";
+                }
+                buildLocation += buildType+"/";
+
+		        // This find 'activity'
+		        for (int index = 0; index < activityList.getLength(); index++) {
+		            Node nNode = activityList.item(index);
+					doInject(nNode, buildLocation);
+		        }
+        	}
+        });
+    }
+
+    private static String getManifestLocation(String flavor, String buildType) {
+		String fileName = "app/build/intermediates/manifests/full/";
+		if (!flavor.equals("")) {
+			fileName += flavor+"/";
+		}
+		fileName += buildType+"/AndroidManifest.xml";
+		System.out.println("Manifest Location : "+fileName);
+
+    	return fileName;
+    }
+
+	private static NodeList getActivityList(String fileName) {
+		// Get Activity List on Manifest.xml
+		//String fileName = "app/build/intermediates/manifests/full/debug/AndroidManifest.xml";
+
+		NodeList activityList = null;
+		try {
+			File fXmlFile = new File(fileName);
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(fXmlFile);
+			doc.getDocumentElement().normalize();
+			String packageName = doc.getDocumentElement().getAttribute("package");
+			packageName = packageName.replace(".", "/");
+
+			// Search Element name that have 'activity' in 'application' 
+			System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+			NodeList applicationList = doc.getElementsByTagName("application");
+			activityList = applicationList.item(0).getChildNodes();
+
+			System.out.println("-----------------------");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return activityList;
+	}
+
+    private static void doInject(Node nNode, String buildLocation) {
+
+        // This find 'activity'
+        if (nNode.getNodeType() == Node.ELEMENT_NODE
+            && nNode.getNodeName().equals("activity")) {
+
+        	// If found activity node do change .class file
+            Element eElement = (Element) nNode;
+        	// TODO 앱의 액티비티인지 패키지명으로 검사할 것 
+            String activityName = getActivityName(eElement);
+            if (activityName != null) {
+            	activityName = activityName.replace(".", "/");
+                System.out.println("Activity name : " + activityName);
+                System.out.println("--- do transform ---");
+
+                System.out.println("Bulid Location : " + buildLocation + activityName);
+				try {
+					new Transformer().doTransform(
+						buildLocation + activityName+".class", 
+						new FileInputStream(buildLocation + activityName + ".class")
+					);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+            }
+        }
+    }
 
     private static String getActivityName(Element eElement) {
         //System.out.println(eElement.getAttributes());
